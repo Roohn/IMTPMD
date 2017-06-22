f68402e957ce8dc6cfea6ff2216309278504fb22
BlackDraugr <daan.vollinga@gmail.com>
2017-06-22 14:43:40 +0200

    intents + doorgeven jaar en semester

--- app/src/main/java/nl/hsleiden/imtpmd/MainActivity.java
+++ app/src/main/java/nl/hsleiden/imtpmd/MainActivity.java
@@ -10,8 +10,11 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 import nl.hsleiden.imtpmd.models.Modules;
+import nl.hsleiden.imtpmd.models.Semester;
 
 public class MainActivity extends AppCompatActivity {
+    int jaar = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoScherm(View v) {
-        Intent intent = new Intent(this, ModulesActivity.class);
+        Intent intent = new Intent(this, SemesterActivity.class);
+        switch (v.getId()) {
+            case R.id.j1:
+                jaar = 1;
+                break;
+            case R.id.j2:
+                jaar = 2;
+                break;
+            case R.id.j3:
+                jaar = 3;
+                break;
+            case R.id.j4:
+                jaar = 4;
+                break;
+        }
+        intent.putExtra("jaar", jaar);
+        Log.d("hoi", ""+jaar);
         startActivity(intent);
     }
 }
